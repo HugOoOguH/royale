c7514012f85c7f4a1c83ffeb8db4d2abf5bca6c4
Hugo <hugo-ensc@outlook.com>
2017-05-17 14:30:33 -0500

    herokuu

--- royale/wsgi.py
+++ royale/wsgi.py
@@ -1,11 +1,6 @@
-
-
 import os
-
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "royale.settings")
-
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 
